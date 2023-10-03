2feee8e54a0d1b197d17ed6749bbc5da1ba8c3cb
shotagvalia <shota.gvalia@gmail.com>
2023-10-03 15:35:20 +0400

    add new lesson12

---
+++ lesson12/index.js
@@ -0,0 +1,60 @@
+// 1
+// const paragraph = document.getElementById("paragraph");
+
+// function changeTextContent() {
+//   paragraph.textContent = "Hello, DOM!";
+// }
+
+// changeTextContent();
+
+// 2 --------------------------
+
+// const button = document.getElementById("btn");
+
+// function changeStyles() {
+//   button.style.backgroundColor = "red";
+//   button.style.color = "white";
+// }
+
+// changeStyles();
+
+// 3 -------------------------------
+// const parent = document.getElementById("parent");
+
+// function createElement() {
+//   const child = document.createElement("p");
+//   child.textContent = "New paragraph";
+//   parent.appendChild(child);
+// }
+
+// createElement();
+
+// 4 ---------------------------
+
+// const button = document.getElementById("btn");
+
+// button.addEventListener("click", handleClick);
+
+// function handleClick() {
+//   alert("Button Clicked");
+// }
+
+// 5 ------------------------------
+
+// const formEl = document.getElementById("form");
+// const inputEl = document.getElementById("input");
+// const errorEl = document.getElementById("error");
+
+// // errorEl.style.display = "none";
+
+// formEl.addEventListener("submit", (event) => {
+//   event.preventDefault();
+
+//   if (inputEl.value.trim() === "") {
+//     // errorEl.style.display = "block";'
+//     errorEl.classList.add("showErorr");
+//   } else {
+//     // errorEl.style.display = "none";
+//     errorEl.classList.remove("showErorr");
+//   }
+// });
